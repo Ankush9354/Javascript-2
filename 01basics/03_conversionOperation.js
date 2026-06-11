56da34a049c1bc4adb7a3027b92f38813ed977fc
ANKUSH <ankushsharma03059@gmail.com>
2026-06-11 14:31:03 +0000

    some coversion change sstudy

--- 01basics/03_conversionOperation.js
+++ 01basics/03_conversionOperation.js
@@ -5,17 +5,17 @@ let score = "hitesh"
 
 let valueInNumber = Number(score)
 //console.log(typeof valueInNumber);
-//console.log(valueInNumber);
+console.log(valueInNumber);
 
 
 // "33" => 33
 // "33abc" => NaN
 //  true => 1; false => 0
 
-let isLoggedIn = "hitesh"
+let isLoggedIn = 0
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-//  console.log(booleanIsLoggedIn);
+ console.log(booleanIsLoggedIn);
 
 // 1 => true; 0 => false
 // "" => false
@@ -31,7 +31,7 @@ let stringNumber = String(someNumber)
 
 let value = 3
 let negValue = -value
-// console.log(negValue);
+console.log(negValue);
 
 // console.log(2+2);
 // console.log(2-2);
@@ -46,15 +46,30 @@ let str2 = " hitesh"
 let str3 = str1 + str2
 // console.log(str3);
 
-// console.log("1" + 2);
-// console.log(1 + "2");
-// console.log("1" + 2 + 2);
-// console.log(1 + 2 + "2");
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
 
 // console.log( (3 + 4) * 5 % 3);
 
-// console.log(+true);
-// console.log(+"");
+console.log(+true);// yeh true -> 1 me covert kr dega aur output 1 dega 
+
+console.log(+""); // yeh 0 output dega
+// yaha pr "+" ka mtlb hai ki jo bhi + ke aage hai vo number me convert ho jayega 
+// +true       // 1
+// +false      // 0
+
+// +"123"      // 123
+// +"12.5"     // 12.5
+
+// +""         // 0
+// +" "        // 0
+
+// +"hello"    // NaN
+
+// +null       // 0
+// +undefined  // NaN
 
 let num1, num2, num3
 
@@ -66,3 +81,9 @@ console.log(gameCounter);
 
 // link to study
 // https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
+
+let number = 1;
+console.log(++number);// 2 -> first increase then return 
+
+console.log(number++); // 2 -> first return then inncrease
+console.log(number); // 3
