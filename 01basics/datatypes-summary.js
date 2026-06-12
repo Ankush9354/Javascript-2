f719407e89c4451298d240383eb859eb95756ac2
ANKUSH <ankushsharma03059@gmail.com>
2026-06-12 10:09:29 +0000

    stack and heap

--- 01basics/datatypes-summary.js
+++ 01basics/datatypes-summary.js
@@ -12,7 +12,9 @@ let userEmail;
 const id = Symbol('123')
 const anotherId = Symbol('123')
 
-console.log(id === anotherId);
+console.log(id === anotherId);// false 
+
+
 
 // const bigNumber = 3456543576654356754n
 
@@ -35,3 +37,6 @@ const myFunction = function(){
 console.log(typeof anotherId);
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
+
+// stack(primitive) and heap(non-primitive)
+
